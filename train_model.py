ba00c7288f9b767f2ed11598bf90f6d12ca3376b
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-01 19:25:51 +0530

    update

---
+++ train_model.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+
